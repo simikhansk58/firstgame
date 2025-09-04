e3e0b969ce8a4460d1e19a4a54c0702dc9cbbbd2
Raees Ahmad Khan <raeesahmadkhan@Raeess-Mac-mini.local>
2025-09-04 19:32:08 +0800

    Update project code

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-screen w-screen bg-gradient-to-b from-blue-50 to-red-100">
+        <div className="h-full w-full bg-gradient-to-b from-blue-50 to-red-100">
           <div className="absolute inset-x-0 top-0 z-50 bg-transparent p-7 text-white">
             <Navbar/>
           </div>
